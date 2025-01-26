bb56caac695030087fa3f5a9d9d417c9c79276ee
nishchal05 <nishchalsundan04@gmail.com>
2025-01-26 23:49:21 +0530

    add dependency

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,9 +1,8 @@
 import { SignIn } from '@clerk/nextjs'
-import banner from "@/public/banner.jpg"
 export default function Page() {
   return <div>
   
-  <img src={banner.src} alt="banner" className=' h-screen w-full'/>
+  <img src="/banner.jpg" alt="banner" className='h-screen w-full' />
   
   <div className=' absolute top-5 right-10'>
   <SignIn />
