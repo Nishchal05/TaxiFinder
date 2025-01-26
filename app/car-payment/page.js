d9d1f8a128278ecee7b783eecdab1c9946533158
nishchal05 <nishchalsundan04@gmail.com>
2025-01-26 23:52:52 +0530

    update car-payment

--- app/car-payment/page.js
+++ app/car-payment/page.js
@@ -1,23 +1,36 @@
-"use client"
+"use client"; 
+
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import { useSearchParams } from 'next/navigation'
-import CheckOutForm from './checkoutform'
-import React from 'react'
+import { useSearchParams } from 'next/navigation';
+import CheckOutForm from './checkoutform';
+import React, { Suspense } from 'react';
+
 const Page = () => {
-  const searchparams=useSearchParams();
-  const amount=searchparams.get('amount');
-  const stripePromise=loadStripe(process.env.NEXT_PUBLIC_PUBLISHABLE_STRIPE_KEY)
-  const options={
-    mode:'payment',
-    amount:Math.round(amount*100),
-    currency:'usd'
-  }
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_PUBLISHABLE_STRIPE_KEY);
+
   return (
-    <Elements stripe={stripePromise} options={options}>
-<CheckOutForm amount={Math.round(amount*100)}/>
-    </Elements>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <PaymentPage stripePromise={stripePromise} />
+    </Suspense>
+  );
 }
 
-export default Page
+const PaymentPage = ({ stripePromise }) => {
+  const searchParams = useSearchParams();
+  const amount = searchParams?.get('amount') || 0;
+  
+  const options = {
+    mode: 'payment',
+    amount: Math.round(amount * 100),
+    currency: 'usd'
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckOutForm amount={Math.round(amount * 100)} />
+    </Elements>
+  );
+};
+
+export default Page;
